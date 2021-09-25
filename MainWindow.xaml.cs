cc4302d6dcbec9d295d8d76df55bedb87f9ed930
Османов Никита <osmanov.nekit@mail.ru>
2021-09-25 16:09:42 +0300

    123

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Variant2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Вы не ввели число");
+            }
+            else
+            {
+                int numb = Convert.ToInt32(textBox1.Text);
+                label1.Content = numb*4;
+                label2.Content = numb * numb;
+
+            }
+        }
+    }
+}
